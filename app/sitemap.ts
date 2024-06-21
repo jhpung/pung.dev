e844a6d6b005209f06c76c922dcb4617e74404ea
jhpung <jhpung.dev@gmail.com>
2024-06-22 00:27:46 +0900

    chore: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,51 @@
+import { fetchNotes } from "@/lib/note";
+import { fetchPosts } from "@/lib/post";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await fetchPosts();
+  const notes = await fetchNotes();
+
+  const postUrls = posts.map(
+    (post) =>
+      ({
+        url: `https://www.pung.dev/posts/${encodeURIComponent(post.slug)}`,
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.7,
+      } as MetadataRoute.Sitemap[number])
+  );
+
+  const noteUrls = notes.map(
+    (note) =>
+      ({
+        url: `https://www.pung.dev/notes/${encodeURIComponent(note.slug)}`,
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.7,
+      } as MetadataRoute.Sitemap[number])
+  );
+
+  return [
+    {
+      url: "https://www.pung.dev",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.pung.dev/posts",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.pung.dev/notes",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    ...postUrls,
+    ...noteUrls,
+  ];
+}
